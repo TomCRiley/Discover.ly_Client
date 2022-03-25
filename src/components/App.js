b6cab3d3e618cd2f66d84c646a4cb09112278028
Ashley Gyngell <ashleygyngell@gmail.com>
2022-03-25 12:43:33 +0000

    beginning of photo upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Register from './Register';
 
 import '../styles/style.scss';
 
 const App = () => (
   <BrowserRouter>
-    <Navbar />
     <Routes>
       <Route path="/" element={<h1>Hello world</h1>} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   </BrowserRouter>
 );
