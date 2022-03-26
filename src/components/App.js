0bbd45d60fb55e911ec826e43bf6a65f57942491
Tom Riley <tom.riley@outlook.com>
2022-03-26 20:37:33 +0000

    added w3w component, implemented api autosuggest and linked to generated map, added icon as pin on map

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import ShowSpot from './ShowSpot';
 import Footer from './Footer';
+import AutoSuggest from './w3w';
 
 import '../styles/style.scss';
 
@@ -13,9 +14,10 @@ const App = () => (
   <BrowserRouter>
     <Navbar />
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/spots/:id" element={<ShowSpot />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/spots/:id' element={<ShowSpot />} />
+      <Route path='/w3w' element={<AutoSuggest />} />
     </Routes>
     <Footer />
   </BrowserRouter>
