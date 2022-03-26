b235fbf72c2f63f73ded1eb39369d9da6b8b9aee
Elise La Rooy <eliselarooy@gmail.com>
2022-03-26 10:28:33 +0000

    added @username to card

--- src/components/SpotCard.js
+++ src/components/SpotCard.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { getUserById } from '../api/spots';
 
-const SpotCard = ({ _id, title, location, img, activity }) => {
+const SpotCard = ({ _id, title, location, img, activity, createdBy }) => {
+  const [user, setUser] = React.useState(null);
+
+  React.useEffect(() => {
+    const getData = async () => {
+      const user = await getUserById(createdBy);
+      setUser(user);
+    };
+
+    getData();
+  }, []);
+
   let icon;
   switch (activity) {
     case 'Running':
@@ -30,19 +42,19 @@ const SpotCard = ({ _id, title, location, img, activity }) => {
             <figure>
               <img src={img} alt={title} />
             </figure>
-          </div>
-
-          <div className="card-header">
-            <div className="card-header-title">
-              <span className="icon">
+            <div className="activity-icon">
+              <span className="icon has-text-white">
                 <i className={`fas ${icon}`}></i>
               </span>
-              <span>{title}</span>
             </div>
           </div>
-
           <div className="card-content">
-            <p className="subtitle">{location}</p>
+            <div className="title is-4">{title}</div>
+            {user ? (
+              <p className="subtitle is-6">{`@${user.username}`}</p>
+            ) : null}
+
+            <p className="subtitle is-6">{location}</p>
           </div>
         </div>
       </Link>
