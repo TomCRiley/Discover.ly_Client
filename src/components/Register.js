c8dc0e1ec7963304fd406288a8fd96963fa6cf1f
Tom Riley <tom.riley@outlook.com>
2022-03-28 10:34:11 +0100

    removed duplicates

--- src/components/Register.js
+++ src/components/Register.js
@@ -79,88 +79,88 @@ export default function Register({ history }) {
 
   return (
     <>
-      <div className="container full-height-content">
-        <h1 className="title">Sign-Up</h1>
-        <label className="label">Profile Picture</label>
-        <div className="container">
-          <div className="columns">
-            <div className="column is-half">
-              <div className="widget">
-                <button className="button" onClick={handleUpload}>
+      <div className='container full-height-content'>
+        <h1 className='title'>Sign-Up</h1>
+        <label className='label'>Profile Picture</label>
+        <div className='container'>
+          <div className='columns'>
+            <div className='column is-half'>
+              <div className='widget'>
+                <button className='button' onClick={handleUpload}>
                   Click to upload a Profile Picture
                 </button>
               </div>
-              <div className="column is-half">
-                <figure className="image is-3by3">
+              <div className='column is-half'>
+                <figure className='image is-3by3'>
                   <img src={imageDisplay} />
                 </figure>
               </div>
             </div>
           </div>
         </div>
-        <div className="container">
+        <div className='container'>
           <form onSubmit={handleSubmit}>
-            <div className="field">
-              <label className="label">Username</label>
-              <div className="control has-icons-left">
+            <div className='field'>
+              <label className='label'>Username</label>
+              <div className='control has-icons-left'>
                 <input
-                  className="input"
-                  type="text"
+                  className='input'
+                  type='text'
                   value={formData.username}
                   onChange={handleChange}
                   name={'username'}
                 />
-                <span className="icon is-left">
-                  <i className="fas fa-user"></i>
+                <span className='icon is-left'>
+                  <i className='fas fa-user'></i>
                 </span>
               </div>
             </div>
-            <div className="field">
-              <label className="label">Email</label>
-              <div className="control has-icons-left">
+            <div className='field'>
+              <label className='label'>Email</label>
+              <div className='control has-icons-left'>
                 <input
-                  className="input"
-                  type="text"
+                  className='input'
+                  type='text'
                   value={formData.email}
                   onChange={handleChange}
                   name={'email'}
                 />
-                <span className="icon is-left">
-                  <i className="fas fa-envelope"></i>
+                <span className='icon is-left'>
+                  <i className='fas fa-envelope'></i>
                 </span>
               </div>
             </div>
-            <div className="field">
-              <label className="label">Password</label>
-              <div className="control has-icons-left">
+            <div className='field'>
+              <label className='label'>Password</label>
+              <div className='control has-icons-left'>
                 <input
-                  className="input"
-                  type="password"
+                  className='input'
+                  type='password'
                   value={formData.password}
                   onChange={handleChange}
                   name={'password'}
                 />
-                <span className="icon is-left">
-                  <i className="fas fa-lock"></i>
+                <span className='icon is-left'>
+                  <i className='fas fa-lock'></i>
                 </span>
               </div>
             </div>
-            <div className="field">
-              <label className="label">Confirm password</label>
-              <div className="control has-icons-left">
+            <div className='field'>
+              <label className='label'>Confirm password</label>
+              <div className='control has-icons-left'>
                 <input
-                  className="input"
-                  type="password"
+                  className='input'
+                  type='password'
                   value={formData.passwordConfirmation}
                   onChange={handleChange}
                   name={'passwordConfirmation'}
                 />
-                <span className="icon is-left">
-                  <i className="fas fa-lock"></i>
+                <span className='icon is-left'>
+                  <i className='fas fa-lock'></i>
                 </span>
               </div>
             </div>
-            <button className="button is-success" onClick={handleSubmit}>
+            <button className='button is-success' onClick={handleSubmit}>
               Submit
             </button>
           </form>
