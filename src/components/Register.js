7b06a6af9cf8367104f0dc069fe8642e66b9b60e
Ashley Gyngell <ashleygyngell@gmail.com>
2022-03-29 17:26:32 +0100

    loeading bar, errors for login

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-import 'bulma';
 import presetDefault from '../assets/images/profileDefault.jpg';
-import { Navigate } from 'react-router-dom';
+import { login } from '../api/auth.js';
+import { useNavigate } from 'react-router-dom';
 
 export default function Register() {
+  const navigate = useNavigate();
   const [imageDisplay, updateImageDisplay] = useState(presetDefault);
   const [formData, updateFormData] = useState({
     username: '',
@@ -15,33 +16,45 @@ export default function Register() {
     passwordConfirmation: '',
   });
 
-  const [errors, updateErrors] = useState({
-    username: '',
+  const [required, updateRequired] = useState({
+    username: '*',
+    email: '*',
+    password: '*',
+    passwordConfirmation: '*',
+  });
+
+  const [loginData, updateLoginData] = React.useState({
     email: '',
     password: '',
-    passwordConfirmation: '',
   });
+
+  const [errorMessage, updateErrorMessage] = useState('');
 
   function handleChange(e) {
     const { name, value } = e.target;
     updateFormData({ ...formData, [name]: value });
-    updateErrors({ ...errors, [name]: '' });
+    updateLoginData({ email: formData.email, password: formData.password });
+    updateRequired({ ...required, [name]: '' });
     console.log(formData);
   }
 
   async function handleSubmit(e) {
     e.preventDefault();
     try {
-      console.log('trying to register', formData);
+      console.log(formData.username.toLowerCase());
       const { data } = await axios.post('/api/register', formData);
-      console.log('Registration Sucess', data);
-      Navigate('/home');
+      console.log('ALERT This is error after post attempt:', data.message);
+      updateErrorMessage(data.message);
     } catch (err) {
-      window.alert('error');
-      console.log('ALERT This is the error ', err.response.data.message);
-      // updateErrors(err.response.data.errors);
-      // window.alert('error');
+      console.log(
+        'ALERT This is an error after failing to post ',
+        err.response.data.message
+      );
     }
+    console.log('THIS IS LOGIN DATA', loginData);
+    await login(loginData);
+
+    navigate('/');
   }
 
   // This uploads all information to Cloudinary
@@ -80,97 +93,115 @@ export default function Register() {
 
   return (
     <>
-      <div className='container full-height-content'>
-        <h1 className='title'>Sign-Up</h1>
-        <label className='label'>Profile Picture</label>
-        <div className='container'>
-          <div className='columns'>
-            <div className='column is-half'>
-              <div className='widget'>
-                <button className='button' onClick={handleUpload}>
+      <div className="pageloader ">
+        <span className="title">Building your account </span>
+      </div>
+      <div className="container full-height-content">
+        <h1 className="title">Sign-Up</h1>
+        <label className="label">Profile Picture</label>
+        <div className="container">
+          <div className="columns">
+            <div className="column is-half">
+              <div className="widget">
+                <button className="button" onClick={handleUpload}>
                   Click to upload a Profile Picture
                 </button>
               </div>
-              <div className='column is-half'>
-                <figure className='image is-3by3'>
-                  <img src={imageDisplay} />
+              <div className="column is-half">
+                <figure className="image is-3by3 ">
+                  <img className="is-rounded" src={imageDisplay} />
                 </figure>
               </div>
             </div>
           </div>
         </div>
-        <div className='container'>
+        <div className="container">
           <form onSubmit={handleSubmit}>
-            <div className='field'>
-              <label className='label'>Username</label>
-              <div className='control has-icons-left'>
+            <div className="field">
+              <label className="label">
+                Username{' '}
+                <small className="has-text-danger">{required.username}</small>
+              </label>
+              <div className="control has-icons-left">
                 <input
-                  className='input'
-                  type='text'
+                  className="input"
+                  placeholder="discoverlyuser"
+                  type="text"
                   value={formData.username}
                   onChange={handleChange}
                   name={'username'}
                 />
-                {
-                  //  IF errors, display error message. If none, dont display.
-                }
-                {errors.username && (
-                  <small className="has-text-danger">{errors.username}</small>
-                )}
+
                 <span className="icon is-left">
                   <i className="fas fa-user"></i>
-
                 </span>
               </div>
             </div>
-            <div className='field'>
-              <label className='label'>Email</label>
-              <div className='control has-icons-left'>
+            <div className="field">
+              <label className="label">
+                Email{' '}
+                <small className="has-text-danger">{required.email}</small>
+              </label>
+
+              <div className="control has-icons-left">
                 <input
-                  className='input'
-                  type='text'
+                  className="input"
+                  placeholder="discoverlyuser@example.com"
+                  type="text"
                   value={formData.email}
                   onChange={handleChange}
                   name={'email'}
                 />
-                <span className='icon is-left'>
-                  <i className='fas fa-envelope'></i>
+
+                <span className="icon is-left">
+                  <i className="fas fa-envelope"></i>
                 </span>
               </div>
             </div>
-            <div className='field'>
-              <label className='label'>Password</label>
-              <div className='control has-icons-left'>
+            <div className="field">
+              <label className="label">
+                Password{' '}
+                <small className="has-text-danger">{required.password}</small>
+              </label>
+              <div className="control has-icons-left">
                 <input
-                  className='input'
-                  type='password'
+                  className="input"
+                  placeholder=""
+                  type="password"
                   value={formData.password}
                   onChange={handleChange}
                   name={'password'}
                 />
-                <span className='icon is-left'>
-                  <i className='fas fa-lock'></i>
+                <span className="icon is-left">
+                  <i className="fas fa-lock"></i>
                 </span>
               </div>
             </div>
-            <div className='field'>
-              <label className='label'>Confirm password</label>
-              <div className='control has-icons-left'>
+            <div className="field">
+              <label className="label">
+                Confirm password{' '}
+                <small className="has-text-danger">
+                  {required.passwordConfirmation}
+                </small>
+              </label>
+              <div className="control has-icons-left">
                 <input
-                  className='input'
-                  type='password'
+                  className="input"
+                  placeholder=""
+                  type="password"
                   value={formData.passwordConfirmation}
                   onChange={handleChange}
                   name={'passwordConfirmation'}
                 />
-                <span className='icon is-left'>
-                  <i className='fas fa-lock'></i>
+                <span className="icon is-left">
+                  <i className="fas fa-lock"></i>
                 </span>
               </div>
             </div>
-            <button className='button is-success' onClick={handleSubmit}>
+            <button className="button is-success" onClick={handleSubmit}>
               Submit
             </button>
+            <small className="has-text-danger"> {errorMessage}</small>
           </form>
           <br />
           <p className="control">
