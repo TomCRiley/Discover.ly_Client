a85df22b20aabcfcdb16c85ec15618e04670b8af
Elise La Rooy <eliselarooy@gmail.com>
2022-04-01 13:50:12 +0100

    profile formatting

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -45,9 +45,9 @@ const UserProfile = () => {
       return <p>No spots...</p>;
     } else {
       return (
-        <section className='scrolling-wrapper'>
+        <section className="scrolling-wrapper">
           {spotsArray.map((spot) => (
-            <div className='cardzz is-rounded' key={spot._id}>
+            <div className="cardzz is-rounded" key={spot._id}>
               <SpotCard key={spot._id} {...spot} />
             </div>
           ))}
@@ -63,56 +63,52 @@ const UserProfile = () => {
   }
 
   return (
-    <section className='hero full-height-content'>
-      <div className='hero-body'>
-        <div className='container'>
-          <div className='columns'>
-            <div className='column is-8 is-offset-2'>
-              <figure className='image is-128x128'>
-                <img
-                  className='is-rounded'
-                  src={user.profileImage}
-                  alt='profile pic'
-                />
-              </figure>
+    <section className="hero full-height-content">
+      <div className="has-background-info p-4 mb-4">
+        <div className="columns py-6">
+          <div className="column is-6 is-offset-2">
+            <div className="content is-medium">
+              <h2 className="subtitle is-5">Hi, friend!</h2>
+              <h1 className="title">@{user.username}</h1>
+              <p className="is-7">
+                When you&apos;re logged in, you get access to all the spots you
+                discovered.
+              </p>
+              <p>Check out the places you&apos;ve liked, too!</p>
             </div>
           </div>
 
-          <section className='section'>
-            <div className='columns'>
-              <div className='column is-8 is-offset-2'>
-                <div className='content is-medium'>
-                  <h2 className='subtitle is-5'>Hi, friend!</h2>
-                  <h1 className='title'>@{user.username}</h1>
-                  <p className='is-7'>
-                    When you&apos;re logged in, you get access to all the spots
-                    you discovered.
-                  </p>
-                  <p>Check out the places you've liked, too!</p>
-                </div>
-              </div>
-            </div>
-          </section>
-
-          <section>
-            <h3 className='title is-5'>Your spots</h3>
-            <div className=''>
-              {!createdSpots ? <p>Loading...</p> : spotsLength(createdSpots)}
-            </div>
-          </section>
-
-          <section className='my-6'>
-            <h3 className='title is-5'>Liked spots</h3>
-            <div className=''>
-              {!likedSpots ? <p>Loading...</p> : spotsLength(likedSpots)}
-            </div>
-          </section>
-
-          <div className='has-text-centered'>
-            <Link to='/discover' className='button is-warning'>
-              Discover more
-            </Link>
+          <div className="column is-2">
+            <figure className="image is-128x128">
+              <img
+                className="is-rounded"
+                src={user.profileImage}
+                alt="profile pic"
+              />
+            </figure>
           </div>
+        </div>
+      </div>
+
+      <div className="container">
+        <section>
+          <h3 className="title is-5">Your spots</h3>
+          <div className="">
+            {!createdSpots ? <p>Loading...</p> : spotsLength(createdSpots)}
+          </div>
+        </section>
+
+        <section className="my-6">
+          <h3 className="title is-5">Liked spots</h3>
+          <div className="">
+            {!likedSpots ? <p>Loading...</p> : spotsLength(likedSpots)}
+          </div>
+        </section>
+
+        <div className="has-text-centered">
+          <Link to="/discover" className="button is-warning">
+            Discover more
+          </Link>
         </div>
       </div>
     </section>
