b00782013e6cbfd47b4cdedf88f035a5a9362cba
Tom Riley <tom.riley@outlook.com>
2022-05-06 14:31:07 +0100

    removed some comments that were irrelevant

--- src/components/Discover.js
+++ src/components/Discover.js
@@ -60,26 +60,26 @@ function Discover() {
 
   return (
     <>
-      <div className="is-flex is-justify-content-center py-6">
+      <div className='is-flex is-justify-content-center py-6'>
         <form onSubmit={handleSubmit}>
-          <div className="field has-addons">
-            <label htmlFor="search" className="label">
-              <div className="control has-icons-left is-rounded">
+          <div className='field has-addons'>
+            <label htmlFor='search' className='label'>
+              <div className='control has-icons-left is-rounded'>
                 <input
-                  type="text"
-                  className="input"
-                  placeholder="Enter search word"
-                  name="text"
+                  type='text'
+                  className='input'
+                  placeholder='Enter search word'
+                  name='text'
                   onChange={handleChange}
                   value={filter.text}
                 />
-                <span className="icon is-left">
-                  <i className="fas fa-search"></i>
+                <span className='icon is-left'>
+                  <i className='fas fa-search'></i>
                 </span>
               </div>
             </label>
-            <div className="control">
-              <button type="submit" className="button is-warning">
+            <div className='control'>
+              <button type='submit' className='button is-warning'>
                 Update results
               </button>
             </div>
@@ -87,33 +87,33 @@ function Discover() {
         </form>
       </div>
 
-      <div className="container full-height-content">
+      <div className='container full-height-content'>
         {data.length === 0 ? (
           <p>No results</p>
         ) : (
           data.map((spot) => (
-            <Link to={`/spots/${spot._id}`} key={spot._id} className="columns">
-              <div className="column is-one-third">
-                <div className="card-image">
-                  <figure className="image ">
+            <Link to={`/spots/${spot._id}`} key={spot._id} className='columns'>
+              <div className='column is-one-third'>
+                <div className='card-image'>
+                  <figure className='image '>
                     <img src={spot.img} alt={spot.title} />
                   </figure>
 
-                  <div className="activity-icon-large">
-                    <span className="icon has-text-white">
+                  <div className='activity-icon-large'>
+                    <span className='icon has-text-white'>
                       <i className={`fas ${getIcon(spot.activity)} fa-xl`}></i>
                     </span>
                   </div>
                 </div>
               </div>
-              <div className="column is-two-thirds">
-                <div className="title">{spot.title}</div>
-                <div className="subtitle">{spot.location}</div>
-                <div className="has-text-black">{spot.description}</div>
+              <div className='column is-two-thirds'>
+                <div className='title'>{spot.title}</div>
+                <div className='subtitle'>{spot.location}</div>
+                <div className='has-text-black'>{spot.description}</div>
 
-                <button className="button is-warning is-inverted">
-                  <span className="icon">
-                    <i className="fas fa-heart"></i>
+                <button className='button is-warning is-inverted'>
+                  <span className='icon'>
+                    <i className='fas fa-heart'></i>
                   </span>
                   <span>{spot.likedBy ? spot.likedBy.length : '0'}</span>
                 </button>
